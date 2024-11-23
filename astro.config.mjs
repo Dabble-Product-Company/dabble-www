5613fed5aeb3b167f12fd0d292db222b2ace0b84
Matt Castillo <mattcastillo2010@gmail.com>
2024-11-23 17:35:41 -0500

    Installed and fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
-import netlify from '@astrojs/netlify';
 
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-minimal-starter.netlify.app/",
   integrations: [sitemap(), tailwind(),],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
 });
