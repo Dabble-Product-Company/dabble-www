51c9990ba5e31ae18b27fed285d36fe68c5d77ee
Matt Castillo <mattcastillo2010@gmail.com>
2024-11-26 16:47:38 -0500

    Favicon generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from '@astrojs/netlify';
 import mdx from '@astrojs/mdx';
+import favicons from 'astro-favicons';
 
 export default defineConfig({
   site: "https://dabble.fyi/",
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(), 
+    mdx(),
+    favicons({
+      masterPicture: "./favicon.png",
+      emitAssets: true,
+      faviconsDarkMode: true,
+    }),
+  ],
   output: 'server',
   adapter: netlify(),
 });
