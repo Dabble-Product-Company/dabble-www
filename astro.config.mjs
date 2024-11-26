bc2f44e4ce01464a298dceb82ed0d6368d9e195c
Matt Castillo <mattcastillo2010@gmail.com>
2024-11-26 17:17:48 -0500

    Tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,20 @@ export default defineConfig({
     favicons({
       masterPicture: "./favicon.png",
       emitAssets: true,
-      faviconsDarkMode: true,
+      appName: "Dabble Product Company",
+      appShortName: "Dabble",
+      appDescription: "Dabble is a product studio building experiences that inspire, empower, and amplify creative expression.",
+      // dir:"auto",
+      lang: "en-US",
+      // display: "standalone",
+      // orientation: "any",
+      // start_url: "/?homescreen=1",
+      background: "#fff",
+      theme_color: "#fff",
+
+      faviconsDarkMode: false, // default `true`, Make favicon compatible with light and dark modes
+      
+      // appleStatusBarStyle: "black-translucent",
     }),
   ],
   output: 'server',
